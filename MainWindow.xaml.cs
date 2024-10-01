b61d6030e812a62607f859c07e3329f7e5a1146c
kimyoungjo <yoyounn88@gmail.com>
2024-10-02 03:20:59 +0900

    feat: 후위표현식을 사용하여 연산자 우선순위 배정

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,8 +7,7 @@ namespace CalculatorAppTest
 {
     public partial class MainWindow : Window
     {
-        private List<double> numbers = new List<double>();
-        private List<string> operations = new List<string>();
+        private List<string> expression = new List<string>();
         private bool isNewNumber = true;
 
         public MainWindow()
@@ -37,14 +36,14 @@ namespace CalculatorAppTest
 
             if (!isNewNumber)
             {
-                numbers.Add(double.Parse(ResultTextBox.Text));
-                operations.Add(op);
+                expression.Add(ResultTextBox.Text);
+                expression.Add(op);
                 ExpressionTextBox.Text += ResultTextBox.Text + " " + op + " ";
                 isNewNumber = true;
             }
-            else if (operations.Count > 0)
+            else if (expression.Count > 0)
             {
-                operations[operations.Count - 1] = op;
+                expression[expression.Count - 1] = op;
                 ExpressionTextBox.Text = ExpressionTextBox.Text.Substring(0, ExpressionTextBox.Text.Length - 2) + op + " ";
             }
         }
@@ -53,36 +52,98 @@ namespace CalculatorAppTest
         {
             if (!isNewNumber)
             {
-                numbers.Add(double.Parse(ResultTextBox.Text));
+                expression.Add(ResultTextBox.Text);
                 ExpressionTextBox.Text += ResultTextBox.Text + " =";
 
-                double result = numbers[0];
-                for (int i = 0; i < operations.Count; i++)
+                double result = CalculateExpression(expression);
+
+                ResultTextBox.Text = result.ToString();
+                expression.Clear();
+                isNewNumber = true;
+            }
+        }
+
+        private double CalculateExpression(List<string> exp)
+        {
+            List<string> postfix = ConvertToPostfix(exp); // 연산자 우선순위를 적용하기위한 후위 표기법으로의 컨버팅
+            Stack<double> stack = new Stack<double>(); // 후위 표기법을 진행하면서 계산값이 계속 저장될 스택 선언
+
+
+            /*
+             foreach문으로 요소를 순회하다가 숫자를 만나면 stack에 push 하고 연산자를 만나면 stack에 쌓인 요소 두개를 꺼내어 
+             해당 연산자에 따른 계산 진행             
+             */
+            foreach (string token in postfix)
+            {
+                if (double.TryParse(token, out double number))
                 {
-                    switch (operations[i])
+                    stack.Push(number);
+                }
+                else
+                {
+                    double b = stack.Pop();
+                    double a = stack.Pop();
+                    switch (token)
                     {
-                        case "+":
-                            result += numbers[i + 1];
-                            break;
-                        case "-":
-                            result -= numbers[i + 1];
-                            break;
-                        case "*":
-                            result *= numbers[i + 1];
-                            break;
+                        case "+": stack.Push(a + b); break;
+                        case "-": stack.Push(a - b); break;
+                        case "*": stack.Push(a * b); break;
                         case "/":
-                            if (numbers[i + 1] != 0)
-                                result /= numbers[i + 1];
+                            if (b != 0)
+                                stack.Push(a / b);
                             else
                                 MessageBox.Show("0으로 나눌 수 없습니다.");
                             break;
                     }
                 }
+            }
 
-                ResultTextBox.Text = result.ToString();
-                numbers.Clear();
-                operations.Clear();
-                isNewNumber = true;
+            return stack.Pop();
+        }
+
+        //중위 표기법을 후위 표기법으로 변환하는 메서드
+        private List<string> ConvertToPostfix(List<string> infix)
+        {
+            List<string> postfix = new List<string>();
+            Stack<string> stack = new Stack<string>();
+
+            foreach (string token in infix)
+            {
+                if (double.TryParse(token, out _))
+                {
+                    postfix.Add(token);
+                }
+                else
+                {
+                    while (stack.Count > 0 && operatorPrecedence(stack.Peek()) >= operatorPrecedence(token))
+                    {
+                        postfix.Add(stack.Pop());
+                    }
+                    stack.Push(token);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                postfix.Add(stack.Pop());
+            }
+
+            return postfix;
+        }
+
+        //연산자의 우선순위를 정하는 메서드
+        private int operatorPrecedence(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                case "-":
+                    return 1;
+                case "*":
+                case "/":
+                    return 2;
+                default:
+                    return 0;
             }
         }
 
@@ -113,8 +174,7 @@ namespace CalculatorAppTest
         {
             ExpressionTextBox.Text = "";
             ResultTextBox.Text = "0";
-            numbers.Clear();
-            operations.Clear();
+            expression.Clear();
             isNewNumber = true;
         }
 
