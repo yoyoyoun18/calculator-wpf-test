3b25eeab0e266fd9ee31544c92d397a29341ce12
kimyoungjo <yoyounn88@gmail.com>
2024-10-02 10:42:28 +0900

    feat: 후위연산 메서드에 괄호 처리 추가

--- Models/CalculatorModel.cs
+++ Models/CalculatorModel.cs
@@ -1,82 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CalculatorAppTest.Models
 {
     public class CalculatorModel
     {
-
-        //연산자 우선순위를 적용하기위해 후위 표현식으로 계산을 진행하는 메서드
-        public double CalculateExpression(List<string> exp)
+        public double CalculateExpression(List<string> expressionList)
         {
-            List<string> postfix = ConvertToPostfix(exp);// 연산자 우선순위를 적용하기위한 후위 표기법으로의 컨버팅
-            Stack<double> stack = new Stack<double>();// 후위 표기법을 진행하면서 계산값이 계속 저장될 스택 선언
-
-            /*
-             foreach문으로 요소를 순회하다가 숫자를 만나면 stack에 push 하고 연산자를 만나면 stack에 쌓인 요소 두개를 꺼내어 
-             해당 연산자에 따른 계산 진행             
-             */
-
-            foreach (string token in postfix)
-            {
-                if (double.TryParse(token, out double number))
-                {
-                    stack.Push(number);
-                }
-                else
-                {
-                    double b = stack.Pop();
-                    double a = stack.Pop();
-                    switch (token)
-                    {
-                        case "+": stack.Push(a + b); break;
-                        case "-": stack.Push(a - b); break;
-                        case "*": stack.Push(a * b); break;
-                        case "/":
-                            if (b != 0)
-                                stack.Push(a / b);
-                            else
-                                throw new DivideByZeroException("0으로 나눌 수 없습니다.");
-                            break;
-                    }
-                }
-            }
-
-            return stack.Pop();
+            var postfixExpression = ConvertToPostfix(expressionList);
+            return EvaluatePostfix(postfixExpression);
         }
 
-        //중위 표기법을 후위 표기법으로 변환하는 메서드
-        private List<string> ConvertToPostfix(List<string> infix)
+        private List<string> ConvertToPostfix(List<string> infixExpression)
         {
-            List<string> postfix = new List<string>();
-            Stack<string> stack = new Stack<string>();
+            var postfix = new List<string>();
+            var operatorStack = new Stack<string>();
 
-            foreach (string token in infix)
+            foreach (var token in infixExpression)
             {
                 if (double.TryParse(token, out _))
                 {
                     postfix.Add(token);
                 }
-                else
+                else if (token == "(")
                 {
-                    while (stack.Count > 0 && OperatorPrecedence(stack.Peek()) >= OperatorPrecedence(token))
+                    operatorStack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
                     {
-                        postfix.Add(stack.Pop());
+                        postfix.Add(operatorStack.Pop());
                     }
-                    stack.Push(token);
+                    if (operatorStack.Count > 0 && operatorStack.Peek() == "(")
+                    {
+                        operatorStack.Pop();
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Mismatched parentheses");
+                    }
+                }
+                else // Operator
+                {
+                    while (operatorStack.Count > 0 && Precedence(operatorStack.Peek()) >= Precedence(token))
+                    {
+                        postfix.Add(operatorStack.Pop());
+                    }
+                    operatorStack.Push(token);
                 }
             }
 
-            while (stack.Count > 0)
+            while (operatorStack.Count > 0)
             {
-                postfix.Add(stack.Pop());
+                if (operatorStack.Peek() == "(")
+                {
+                    throw new InvalidOperationException("Mismatched parentheses");
+                }
+                postfix.Add(operatorStack.Pop());
             }
 
             return postfix;
         }
 
-        //연산자의 우선순위를 정하는 메서드
-        private int OperatorPrecedence(string op)
+        private int Precedence(string op)
         {
             switch (op)
             {
@@ -85,10 +73,53 @@ namespace CalculatorAppTest.Models
                     return 1;
                 case "*":
                 case "/":
+                case "%":
                     return 2;
                 default:
                     return 0;
             }
+        }
+
+        private double EvaluatePostfix(List<string> postfixExpression)
+        {
+            var stack = new Stack<double>();
+
+            foreach (var token in postfixExpression)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    if (stack.Count < 2)
+                    {
+                        throw new InvalidOperationException("Invalid expression");
+                    }
+                    double b = stack.Pop();
+                    double a = stack.Pop();
+                    switch (token)
+                    {
+                        case "+": stack.Push(a + b); break;
+                        case "-": stack.Push(a - b); break;
+                        case "*": stack.Push(a * b); break;
+                        case "/":
+                            if (b == 0) throw new DivideByZeroException();
+                            stack.Push(a / b);
+                            break;
+                        case "%": stack.Push(a % b); break;
+                        default:
+                            throw new InvalidOperationException($"Unknown operator: {token}");
+                    }
+                }
+            }
+
+            if (stack.Count != 1)
+            {
+                throw new InvalidOperationException("Invalid expression");
+            }
+
+            return stack.Pop();
         }
     }
 }
